1d34a36c426d028af6f627e882bd15e58f01377e
自分のアカウント名 <>
2022-12-17 16:04:51 +0900

    mojikan

--- mojiretu_kansu.py
+++ mojiretu_kansu.py
@@ -1,4 +1,8 @@
-def mojiretu_kansu(mojiretu,yoko_suji,tate_suji):
+def moji_kan(mojiretu,yoko_suji,tate_suji):
     len_str = len(param_str)
 
     if not (MIN_LEN_STR <= len_str <= MAX_LEN_STR):
+        pass
+
+if __name__ == "__main__":
+    moji_kan("mojiretu",1,12)
