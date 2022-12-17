a30f721e7b19f17f0d35c783843677837d040dd6
自分のアカウント名 <>
2022-12-17 17:19:48 +0900

    mojikankomento

--- mojiretu_kansu.py
+++ mojiretu_kansu.py
@@ -1,8 +1,17 @@
 def moji_kan(mojiretu,yoko_suji,tate_suji):
+    """
+    横と縦にパラメータの数値を繰り返した文字列を返却する関数。
+
+    Args:
+        mojiretu (_type_): 文字列
+        yoko_suji (_type_): 横のパラメータ
+        tate_suji (_type_): 縦のパラメータ
+    """
     len_str = len(param_str)
 
     if not (MIN_LEN_STR <= len_str <= MAX_LEN_STR):
         pass
+    """TODO:処理未実装"""
 
 if __name__ == "__main__":
     moji_kan("mojiretu",1,12)
